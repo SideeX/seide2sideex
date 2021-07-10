e38ab55c461dc45c1ad6ba767d6555bbb97fd336
yijucho <steven140799@gmail.com>
2021-07-10 22:45:05 +0800

    Update a default function to test

---
+++ src/recordCommand/defaultFunc.ts
@@ -0,0 +1,34 @@
+import { Command } from '../struct/seleniumStruct';
+import { Record } from '../struct/sideexStruct';
+
+export function defaultFunc(
+    seleniumCommand: Command,
+    boolOrNot: boolean,
+): Record {
+    const sideexRecord: Record = {
+        name: seleniumCommand.command,
+        target: {
+            usedIndex: 1,
+            options: [
+                {
+                    type: 'tempType',
+                    value: 'tempValue',
+                },
+            ],
+            tac: 'tempTac',
+        },
+        value: {
+            usedIndex: 1,
+            options: [
+                {
+                    type: 'tempType',
+                    value: 'tempValue',
+                },
+            ],
+            tac: 'tempTac',
+        },
+        pwt: { temp: 1 },
+        comment: boolOrNot,
+    };
+    return sideexRecord;
+}
