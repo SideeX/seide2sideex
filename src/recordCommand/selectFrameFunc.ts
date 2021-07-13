47a9aeca3d08124ff79e476274df30266aa0d667
allen_YF_Chen <allen_YF_Chen>
2021-07-14 00:19:46 +0800

    feat: add selectFrameFunc

---
+++ src/recordCommand/selectFrameFunc.ts
@@ -0,0 +1,34 @@
+import { Command } from '../struct/seleniumStruct';
+import { Record } from '../struct/sideexStruct';
+
+export function selectFrameFunc(
+    seleniumCommand: Command,
+    isCommandComment: boolean,
+): Record {
+    const sideexRecord: Record = {
+        name: seleniumCommand.command,
+        target: {
+            usedIndex: 0,
+            options: [
+                {
+                    type: 'other',
+                    value: seleniumCommand.target,
+                },
+            ],
+            tac: '',
+        },
+        value: {
+            usedIndex: 0,
+            options: [
+                {
+                    type: 'other',
+                    value: '',
+                },
+            ],
+            tac: '',
+        },
+        pwt: { pbw: 0, paw: 0, prw: 0, pdw: 0 },
+        comment: isCommandComment,
+    };
+    return sideexRecord;
+}
