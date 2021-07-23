ebc6779af9bff5e9480b6995c51ea6e7b4800be8
yb999 <lojobs38@gmail.com>
2021-07-23 17:00:13 +0800

    feat: add cli feature, now can type file-path on command line

---
+++ bin/cli.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+const { program } = require('commander');
+const { mainFunc } = require('../build/main');
+
+program
+  .description('An cli to read path')
+  .option('-p, --path <type>', 'Type the path');
+
+program.addHelpText(
+  'after',
+  `
+Example usage:
+  $ seide2sideex-cli -p path-of-the-(.side)
+  $ seide2sideex-cli -p seleniumFile/open.side`,
+);
+
+program.parse();
+
+const options = program.opts();
+console.log('The path is:');
+
+if (options.path) {
+    console.log(`Your .side file path on: ./${options.path}`);
+    mainFunc(`${options.path}`);
+}
+if (process.argv.length < 3) {
+  program.help()
+}
