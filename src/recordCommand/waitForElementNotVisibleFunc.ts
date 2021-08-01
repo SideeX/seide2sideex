7479cd657efd6f16551db87713dc91f26a82f3db
tyler <tyler88128949@gmail.com>
2021-08-01 18:43:42 +0800

    feat: add waitForElementPresentFunc, waitForElementVisibleFunc, waitForElementNotPresentFunc, waitForElementNotVisibleFunc,

---
+++ src/recordCommand/waitForElementNotVisibleFunc.ts
@@ -0,0 +1,41 @@
+import { Record } from '../struct/sideexStruct';
+import { ConvertFuncParameter } from '../struct/convertFuncParameterStruct';
+
+export function waitForElementNotVisibleFunc(
+    parameters: ConvertFuncParameter,
+): Record {
+    const seleniumCommand = parameters.command;
+    const isCommandComment = parameters.isCommandComment;
+
+    const sideexRecord: Record = {
+        name: 'waitForElementNotVisible',
+        target: {
+            usedIndex: 0,
+            options: [
+                {
+                    type: 'other',
+                    value: seleniumCommand.target,
+                },
+            ],
+            tac: '',
+        },
+        value: {
+            usedIndex: 0,
+            options: [
+                {
+                    type: 'other',
+                    value: seleniumCommand.value,
+                },
+            ],
+            tac: '',
+        },
+        pwt: {
+            pbw: 0,
+            paw: 0,
+            prw: 0,
+            pdw: 0,
+        },
+        comment: isCommandComment,
+    };
+    return sideexRecord;
+}
